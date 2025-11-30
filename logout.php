9c315516d391160022cb0b51d725882d80a69749
NanaKY931 <nana.yirenkyi@ashesi.edu.gh>
2025-11-30 16:08:47 +0000

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'db_connect.php';  // Must check with the database regardless, as it must search and destroy user credentials from the database.
+
 // PHP Script 6: Logout Utility (logout.php)
 session_start();
 
@@ -9,6 +11,6 @@ $_SESSION = array();
 session_destroy();
 
 // Redirect to the login page
-header("Location: login.html");
+header("Location: signup.html");
 exit;
 ?>
